b9f847c0315621df36653ebc1546de9e5dd0124d
pdpatel2 <pdpatel2@gmail.com>
2015-12-24 15:17:42 -0500

    bug fix

--- router.js
+++ router.js
@@ -18,7 +18,7 @@ function home(request, response) {
 		//if the url == "/" and POST
 
 		//get post data from body
-		request.on("data", function(postBody){
+		request.on("data", function (postBody){
 			//extract username
 			var query = querystring.parse(postBody.toString());
 
@@ -33,20 +33,20 @@ function home(request, response) {
 //Handle HTTP route for GET / :username i.e. .chalkers
 function user(request,response) {
 	//if the url == "/..."
-	var username = request.url.replace('/', ' ');
+	var username = request.url.replace('/', '');
 	if(username.length > 0) {
 		response.writeHead(200, commonHeaders);
 		renderer.view('header', {}, response);
 
 		//get json from Treehouse
-		var studentProfile = new Profile("username");
+		var studentProfile = new Profile(username);
 		//on "end" (when all data comes back)		
 		studentProfile.on("end", function(profileJSON){
 			//show profile
 
 			//store the values which we need
 			var values = {
-				avatarUrl: profileJSON.gravatar_url,
+				avatarURL: profileJSON.gravatar_url,
 				username: profileJSON.profile_name,
 				badges: profileJSON.badges.length,
 				javascriptPoints: profileJSON.points.JavaScript
